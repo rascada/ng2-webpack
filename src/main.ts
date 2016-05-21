bf8b2ba14122c48f68db61f3bd8c3506774626c0
rascada <carftjr@gmail.com>
2016-05-22 00:38:52 +0200

    update to angular-rc

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@
 
 import 'core-js';
 import 'zone.js';
-import { bootstrap } from 'angular2/platform/browser';
+import {bootstrap} from '@angular/platform-browser-dynamic';
 
-import AppComponent from './app.component';
+import {AppComponent} from './app.component';
 
 bootstrap(AppComponent);
